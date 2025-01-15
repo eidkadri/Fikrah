40ef2b5b15df1954ce9dd8fe37121ea04591a0c6
eidkadri <121609986+eidkadri@users.noreply.github.com>
2025-01-15 19:12:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,13 +58,12 @@ app.use((req, res) => {
 });
 
 // اتصال بـ MongoDB Atlas
-mongoose.connect('mongodb://127.0.0.1:27017/Bookstore', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('✅ Successfully connected to MongoDB'))
-.catch((err) => console.error('❌ Failed to connect to MongoDB:', err.message));
-
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 // تشغيل الخادم
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
