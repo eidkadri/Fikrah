31e341cee1571423e57005d1eb6b9d483601f58a
eidkadri <121609986+eidkadri@users.noreply.github.com>
2025-01-17 19:41:06 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const router = express.Router();
 
-app.get('/endpoint', (req, res) => {
+router.get('/endpoint', (req, res) => {
     const message = { greeting: "السلام عليكم" };
     res.json(message);
 });
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = router;
