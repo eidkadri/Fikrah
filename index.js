0ba725bd1e51c8de687ea2c97f8a557035eea1a1
eidkadri <121609986+eidkadri@users.noreply.github.com>
2025-01-17 17:09:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,10 +62,10 @@ app.use((req, res) => {
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Failed to connect to MongoDB', err));
-//تاكد من الاتصال
+}).then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+// تحقق من الاتصال بقاعدة البيانات
 mongoose.connection.on('connected', () => {
     console.log('MongoDB connected successfully');
 });
@@ -73,6 +73,7 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.error('MongoDB connection error:', err);
 });
+
 
 // تشغيل الخادم
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
